cf9e4d6fbc9a8d48ee6913a50e02e73b36e706da
iasvobodin <iasvobodin@gmail.com>
2023-11-15 00:34:52 +0500

    ыфму

--- src/commands/error.ts
+++ src/commands/error.ts
@@ -1,18 +1,14 @@
 import { Context, Markup } from 'telegraf';
 
 const error = () => async (ctx: Context) => {
-  const errorText = 'Ваш текст замечаний здесь...';
+  const errorText = 'Добавление ошибки на шкаф';
 
   const keyboard = Markup.inlineKeyboard([
-      Markup.button.callback('Создать новый', 'create_new'),
-      Markup.button.callback('Выбрать текущий', 'choose_current'),
+    Markup.button.callback('Создать новый', 'create_new'),
+    Markup.button.callback('Выбрать текущий', 'choose_current'),
   ]);
 
-  const additionalButtons = Markup.inlineKeyboard([
-    Markup.button.callback('Заявка на производство', 'production_request'),
-    Markup.button.callback('Обозначение изделия', 'product_designation'),
-    Markup.button.callback('Чертёжный индекс изделия', 'technical_index'),
-]);
+
 
   ctx.reply(errorText, keyboard);
 
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import { Telegraf } from 'telegraf';
-
+import { Telegraf, Markup, Context } from 'telegraf';
+import { sql } from '@vercel/postgres';
+import { code } from 'telegraf/format'
 import { about } from './commands';
 import { error } from './commands';
 import { greeting } from './text';
@@ -13,25 +14,121 @@ const bot = new Telegraf(BOT_TOKEN);
 
 bot.command('about', about());
 bot.command('error', error());
-bot.on('message', greeting());
+// bot.on('message', greeting());
+
+const additionalButtons = Markup.inlineKeyboard([
+	Markup.button.callback('Заявка на производство', 'production_request'),
+	Markup.button.callback('Обозначение изделия', 'product_designation'),
+	Markup.button.callback('Чертёжный индекс изделия', 'technical_index'),
+]);
+
+interface UserState {
+	stage: number;
+	productionRequest?: string;
+	productDesignation?: string;
+	technicalIndex?: string;
+}
+
+let userState: { [userId: number]: UserState } = {};
+
+bot.action('create_new', (ctx: Context) => {
+	ctx.editMessageText('Добавляем новый шкаф.\nВведите\nЗаявку на производство:');
+	userState[ctx.from!.id] = { stage: 1 };
+});
+
+bot.on('text', async (ctx) => {
+	const userId = ctx.from.id;
+	const currentState = userState[userId];
+
+	if (currentState) {
+		switch (currentState.stage) {
+			case 1:
+				currentState.productionRequest = ctx.message.text;
+				currentState.stage++;
+				ctx.reply('Введите Обозначение изделия:');
+				break;
+			case 2:
+				currentState.productDesignation = ctx.message.text;
+				currentState.stage++;
+				ctx.reply('Введите Чертёжный индекс изделия:');
+				break;
+			case 3:
+				currentState.technicalIndex = ctx.message.text;
+
+				// Добавление в базу данных
+				// await ctx.reply(code('save on db'))
+				ctx.replyWithHTML('<i>Обработка запроса...</i>');
+				await sql`INSERT INTO Pets (Name, Owner) VALUES (${ctx.message.text}, ${ctx.message.text});`;
+
+
+				ctx.reply('Новый шкаф добавлен в бд');
+				delete userState[userId]; // Очистка состояния
+				break;
+
+		}
+	}
+});
+
+
+
+
+
 
 bot.action('create_new', (ctx) => {
-  ctx.reply('Вы выбрали "Создать новый".', 	{
-		reply_markup: {
-			force_reply: true,
-			input_field_placeholder: "Reply with your answer",
-		},
-	},);
+	ctx.editMessageText('Новый шкаф в бд', additionalButtons);
 });
 
 bot.action('choose_current', (ctx) => {
-  ctx.reply('Вы выбрали "Выбрать текущий".');
+	// ctx.reply('Вы выбрали "Выбрать текущий".');
+	ctx.reply('Заявка на производство:');
+	bot.on('text', async (ctx) => {
+		await sql`INSERT INTO Pets (Name, Owner) VALUES (${ctx.message.text}, ${ctx.message.text});`;
+	});
+
+	ctx.reply('Обозначение изделия:');
+	bot.on('text', async (ctx) => {
+		await sql`INSERT INTO Pets (Name, Owner) VALUES (${ctx.message.text}, ${ctx.message.text});`;
+	});
+
+	ctx.reply('Чертёжный индекс изделия:');
+	bot.on('text', async (ctx) => {
+		await sql`INSERT INTO Pets (Name, Owner) VALUES (${ctx.message.text}, ${ctx.message.text});`;
+	});
+	ctx.reply('Шкаф добавлен в бд');
+
 });
+
+
+bot.action('production_request', async (ctx: Context) => {
+	ctx.reply('Введите Заявку на производство:');
+	bot.on('text', async (ctx) => {
+		await sql`INSERT INTO Pets (Name, Owner) VALUES (${ctx.message.text}, ${ctx.message.text});`;
+		// const note = new Note({ text: ctx.message.text });
+		// await note.save();
+		ctx.reply('Замечание сохранено в базе данных.');
+	});
+	// ctx.editMessageText('Введите Заявку на производство:');
+	// const messageId = ctx.message?.message_id;
+
+	// bot.on('message', greeting());
+});
+
+bot.action('product_designation', (ctx) => {
+	ctx.reply('Введите Обозначение изделия:');
+});
+
+bot.action('technical_index', (ctx) => {
+	ctx.reply('Введите Чертёжный индекс изделия:');
+});
+
+
+
+
 
 
 //prod mode (Vercel)
 export const startVercel = async (req: VercelRequest, res: VercelResponse) => {
-  await production(req, res, bot);
+	await production(req, res, bot);
 };
 //dev mode
 ENVIRONMENT !== 'production' && development(bot);
